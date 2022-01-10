0fb495f2d605ef42a2cbd47bbc3e74d6ea81c337
yamato0211 <kumayama0211@gmail.com>
2022-01-10 16:17:34 +0900

    今日2回目のコミット

--- src/App.js
+++ src/App.js
@@ -4,15 +4,35 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [count3, setCount3] = useState(false)
+  const [Mode, setMode] = useState('normalCountUpMode')
+  const Btn3 = document.getElementById('btn3')
 
   const countUp = () => {
-    setCount(count => count + 1);
+    if (count3) {
+      setCount(count => count + 3)
+    } else {
+      setCount(count => count + 1)
+    }
   }
   const countDown = () => {
-    if (count > 0) {
+    if (count > 0 && count3 === false) {
       setCount(count => count - 1);
+    } else if (count > 2 && count3 === true) {
+      setCount(count => count - 3)
     } else {
-      alert("countDown Error");
+      alert('CountDown Error')
+    }
+  }
+  const count3Up = () => {
+    if (count3) {
+      setCount3(false)
+      Btn3.textContent = "3倍CountUpModeへ移行"
+      setMode('normalCountUpMode')
+    } else {
+      setCount3(true)
+      Btn3.textContent = 'normalCountUpModeへ移行'
+      setMode('3倍CountUpMode')
     }
   }
 
@@ -20,8 +40,11 @@ function App() {
     <div className="App">
       <p>Hello React</p>
       <p>corrent Num : {count}</p>
+      <p>現在は{Mode}</p>
+      <p>0より小さくなる時にエラーが出ます</p>
       <button onClick={countUp}>count Up!</button>
       <button onClick={countDown}>count Down!</button>
+      <button onClick={count3Up} id='btn3'>3倍CountUpModeへ移行</button>
     </div>
   );
 }
